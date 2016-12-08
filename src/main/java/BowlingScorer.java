0e4213c401adc9943e8cde5987a16ad59a14a5e6
Abraham Vallez <abevallez@gmail.com>
2016-12-08 21:18:40 +0100

    refactor prod code

--- src/main/java/BowlingScorer.java
+++ src/main/java/BowlingScorer.java
@@ -5,32 +5,49 @@
  */
 public class BowlingScorer {
 
+    static final int SPARE_VALUE = 9;
+
     /**
      * Produce the total Score from a Bowling Game
      *
-     * @param game String
+     * @param game
      * @return int
      */
     public int totalScoreFromAGame(String game) {
         int i;
         char roll;
-        char previousRoll;
         int totalScore = 0;
+        int previousSum = 0;
+        int pinDowns;
 
         for (i=0; i<game.length(); i++) {
             roll = game.charAt(i);
-            if (Character.isDigit(roll)) {
-                totalScore += Character.getNumericValue(roll);
-            } else if (roll == '/') {
-                previousRoll = game.charAt(i-1);
-                if (Character.isDigit(previousRoll)) {
-                    totalScore -= Character.getNumericValue(previousRoll);
-                }
-                totalScore += 9;
+            if (roll == '/') {
+                totalScore = sumSpare(totalScore, previousSum);
+                previousSum = 0;
+            }else if (Character.isDigit(roll)) {
+                pinDowns = Character.getNumericValue(roll);
+                totalScore += pinDowns;
+                previousSum = pinDowns;
+            } else {
+                previousSum = 0;
             }
-
         }
 
+        return totalScore;
+    }
+
+    /**
+     * Sum Spare to totalScore correctly.
+     *
+     * @param totalScore
+     * @param previousSum
+     *
+     * @return totalScore
+     */
+    protected int sumSpare(int totalScore, int previousSum) {
+        totalScore -= previousSum;
+        totalScore += SPARE_VALUE;
         return totalScore;
     }
 }
