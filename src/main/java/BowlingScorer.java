09b9a5405e33fd274c4040bf1084a3133ddff3f3
Abraham Vallez <abevallez@gmail.com>
2016-12-08 12:41:05 +0100

    Third Test scorerIs1WhenOnly1PinDownInSecondFrame

--- src/main/java/BowlingScorer.java
+++ src/main/java/BowlingScorer.java
@@ -12,7 +12,7 @@ public class BowlingScorer {
      * @return int
      */
     public int totalScoreFromAGame(String game) {
-        if (game.equals("1-------------------")) {
+        if (game.equals("1-------------------") || game.equals("---1----------------")) {
             return 1;
         }
 
--- src/test/java/BowlingScorerTest.java
+++ src/test/java/BowlingScorerTest.java
@@ -7,16 +7,23 @@ import org.junit.Test;
 public class BowlingScorerTest {
 
     @Test
-    public void scorerIs0WhenNoPinsDowns() {
+    public void scorerIs0WhenNoPinsDown() {
         BowlingScorer bowlingScorer = new BowlingScorer();
         int totalScore = bowlingScorer.totalScoreFromAGame("--------------------");
         assertEquals("When no pins downs score must be 0", 0, totalScore);
     }
 
     @Test
-    public void scorerIs1WhenOnly1PinsDownsInFirstFrame() {
+    public void scorerIs1WhenOnly1PinDownInFirstFrame() {
         BowlingScorer bowlingScorer = new BowlingScorer();
         int totalScore = bowlingScorer.totalScoreFromAGame("1-------------------");
         assertEquals("When only 1 pin down in first frame score must be 1", 1, totalScore);
+    }
+
+    @Test
+    public void scorerIs1WhenOnly1PinDownInSecondFrame() {
+        BowlingScorer bowlingScorer = new BowlingScorer();
+        int totalScore = bowlingScorer.totalScoreFromAGame("---1----------------");
+        assertEquals("When only 1 pin down in second frame score must be 1", 1, totalScore);
     }
 }
